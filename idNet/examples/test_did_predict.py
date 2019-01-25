66f77643e7234a7428423cb4ba2e9865e0d25570
Jonathan Dunn <jdunn8@iit.edu>
2019-01-26 09:53:40 +1300

    clean-up

--- idNet/examples/test_did_predict.py
+++
@@ -1,7 +0,0 @@
-from idNet import idNet_Enrich
-
-test_texts = ["This is the first sentence just in case", "But this is actually the second English sentence."]
-
-id = idNet_Enrich("Model.eng.DID.SVM.eng.p", language = "eng")
-results = id.predict(test_texts)
-print(results)	
--- idNet/examples/test_lid_predict.py
+++
@@ -1,7 +0,0 @@
-from idNet import idNet_Enrich
-
-test_texts = ["This is the first sentence just in case", "But this is actually the second English sentence."]
-
-id = idNet_Enrich("Model.LID.MLP.400kx3_hash.1-3grams.262k.Callback.hdf")
-results = id.predict(test_texts)
-print(results)
